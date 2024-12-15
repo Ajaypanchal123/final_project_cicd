52000b2d73171722f9733acc1f7df854210de02e
Ajaypanchal123 <panchalajay018@gmail.com>
2024-12-14 23:34:59 -0500

    yml file updated

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,9 @@
 import pytest
 from app.app import app
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
+
 
 @pytest.fixture
 def client():
